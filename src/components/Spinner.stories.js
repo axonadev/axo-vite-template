fccafa9b3eb5c3067ae7776b4466fee86ef88389
Adekro <e.croce88@gmail.com>
2024-12-06 17:15:29 +0100

    add spinner storybook

--- src/components/Spinner.stories.js
+++ src/components/Spinner.stories.js
@@ -4,7 +4,7 @@ import Spinner from "./Spinner";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: "Example/Spinner",
+  title: "Example/Components/Spinner",
   component: Spinner,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -13,9 +13,20 @@ export default {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    borderColor: { control: "color" },
+    animationTime: { control: "text" },
+  },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
-  args: {},
+  args: { borderColor: "#c2c2c2" },
+};
+
+export const BorderRed = {
+  args: {
+    borderColor: "red",
+    animationTime: "2s",
+  },
 };
