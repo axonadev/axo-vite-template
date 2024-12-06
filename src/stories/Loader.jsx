163d18ac78b94fab3f9eeedf97bd9c62e80f4373
Adekro <e.croce88@gmail.com>
2024-12-06 16:45:49 +0100

    add storybook

---
+++ src/stories/Loader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Loader from "../components/Loader";
+
+/** Primary UI component for user interaction */
+export const DocLoader = () => {
+  return <Loader />;
+};
---
+++ src/stories/Spinner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import DocSpinner from "../components/Spinner";
+
+/** Primary UI component for user interaction */
+export const Spinner = () => {
+  return <DocSpinner />;
+};
